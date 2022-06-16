bb450b1c00a46df9524ba25a0c5c325fa9735760
Oliver Chang <ochang@xpanxion.com>
2022-06-15 20:59:35 -0400

    second commit

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) {
-
+        //Create scanner object
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter a: ");
         int a = input.nextInt();
         System.out.print("Enter b: ");
@@ -13,30 +14,32 @@ public class Problem1 {
         System.out.print("Enter d: ");
         int d = input.nextInt();
 
+        // Call methods
         sum(a,b,c,d);
         multiple(a,b,d);
         division(b,d);
         subtraction(b,c);
     }
-    public static int sum(int a ,int b, int c, int d)
+    // Define sum, multiple, division and subtraction methods
+    public static void sum(int a ,int b, int c, int d)
     {
         int results = a + b + c+ d;
-        System.out.println(a+"+"+b+"+"+c +"+"+d+"="+results);
-        return results;
+        System.out.println(a + " + " + b + " + " + c + " + " + d + " = " + results);
     }
     public static void multiple(int a, int b, int d)
-    {int results = a * b * d;
-        System.out.println(a+"*"+b+"*"+d +"="+results);
+    {
+        int results = a * b * d;
+        System.out.println(a + "*" + b + "*" + d + "=" + results);
     }
     public static void division(int b, int d)
-    {double results = (double) d / b;
-        System.out.println(d+"/"+b +"="+results);
-
-        }
+    {
+        double results = (double) d / b;
+        System.out.println(d + "/" + b + "=" + results);
+    }
     public static void subtraction(int b, int c)
     {
         int results =  b-c;
-        System.out.println(b+"-"+c +"="+results);
+        System.out.println(b + "-" + c + "=" + results);
     }
 }
 
